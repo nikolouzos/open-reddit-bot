183ae9fbe7222dc5910e023dbc3e144690fd1b75
Nikolaos Rafail Nikolouzos <nickrnikolouzos@gmail.com>
2019-09-11 22:14:36 +0300

    Added the Vuetify framework

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import Vue from 'vue';
+import 'vue-resource';
 import App from './App.vue';
 import router from './router';
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => (h(App)),
+  vuetify,
+
+  // vue-resource config
+  http: {
+    root: 'https://www.reddit.com/dev/api',
+    headers: {
+      Authorization: '',
+    },
+  },
 }).$mount('#app');
